8209c44ff556d7d1f1a512299cc268e8849f5f4e
cointaft <148601710+cointaft@users.noreply.github.com>
2023-12-11 09:27:55 +0700

    new vSporkAddresses

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -624,7 +624,7 @@ public:
         nPoolNewMaxParticipants = 20;
         nFulfilledRequestExpireTime = 60*60; // fulfilled requests expire in 1 hour
 
-        vSporkAddresses = {"TVjkd8bSveaBgYFV59QdvQ2KXc2nFPcTS7"};
+        vSporkAddresses = {"TG7W1AUqwCmEPvdFkihRNJT5Jm7fXC1Vox"};
         nMinSporkKeys = 1;
         fBIP9CheckSmartnodesUpgraded = true;
 
